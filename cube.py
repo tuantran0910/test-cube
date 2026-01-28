64d8094bb1d94d3c13b9f8416c7482d7c37b56de
tuantran0910 <tntuan0910@gmail.com>
2026-01-28 20:30:07 +0700

    feat: init repo
    Signed-off-by: tuantran0910 <tntuan0910@gmail.com>

---
+++ cube.py
@@ -0,0 +1,14 @@
+import uuid
+
+from cube import config  # type: ignore[attr-defined]
+from cube import file_repository  # type: ignore[attr-defined]
+
+
+@config("repository_factory")
+def repository_factory(ctx: dict) -> list[dict]:
+    return file_repository("models")
+
+
+@config("schema_version")
+def schema_version(ctx: dict) -> str:
+    return str(uuid.uuid4())[:8]
